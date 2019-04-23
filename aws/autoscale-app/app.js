b092d61003b7ef39be94134f5bb079c45553b109
Mohamed Sahad KP <mohamedsahadkp@gmail.com>
2019-04-23 17:08:11 +0530

    added autoscaling module for k8s pods

---
+++ aws/autoscale-app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express'),
+app = express();
+
+app.get('/api', function(req, res) {
+  res.send({
+    "status" : "OK",
+    "message" : "Autoscale APIs are working" 
+  });
+});
+
+app.listen(8080);
