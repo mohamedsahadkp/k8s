cabc2019b648c6f2957dca5dc3c2389c71ff47f5
Mohamed Sahad KP <mohamedsahadkp@gmail.com>
2019-04-23 20:09:57 +0530

    add loadgenerator api (fibo)

--- aws/autoscale-app-2/app.js
+++ aws/autoscale-app-2/app.js
@@ -4,10 +4,20 @@ var express = require('express'),
 app = express();
 
 app.get('/api', function(req, res) {
+  if(req.query.fibo != undefined) {
+    let num = parseInt(req.query.fibo);
+    console.log("Result :" + fibo(num));
+  }
   res.send({
     "status" : "OK",
-    "message" : "Autoscale APIs are working 2" 
+    "message" : "Autoscale APIs are working 3" 
   });
 });
+
+function fibo(n) {
+  if (n < 2)
+      return 1;
+  else   return fibo(n - 2) + fibo(n - 1);
+}
 
 app.listen(9595);
